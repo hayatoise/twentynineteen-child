5ce62bdf27bd604cebc711b2717921c23df00ae0
Hayato Ise <hayatoise.com@gmail.com>
2019-01-21 05:24:13 +0900

    feat: Add excerpt form top page

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -12,22 +12,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_sticky() && is_home() && ! is_paged() ) {
-			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
-		}
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		endif;
-		?>
-	</header><!-- .entry-header -->
+    <header class="entry-header">
+        <?php
+        if (is_sticky() && is_home() && !is_paged()) {
+            printf('<span class="sticky-post">%s</span>', _x('Featured', 'post', 'twentynineteen'));
+        }
+        if (is_singular()) :
+            the_title('<h1 class="entry-title">', '</h1>');
+        else :
+            the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
+        endif;
+        ?>
+    </header><!-- .entry-header -->
 
-	<?php twentynineteen_post_thumbnail(); ?>
+    <?php twentynineteen_post_thumbnail(); ?>
 
-	<footer class="entry-footer">
-		<?php twentynineteen_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <div class="entry-content">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-content -->
+
+    <footer class="entry-footer">
+        <?php twentynineteen_entry_footer(); ?>
+    </footer><!-- .entry-footer -->
 </article><!-- #post-${ID} -->
